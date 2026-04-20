949d9308c8f75ac845a821b2a8aeee4be536c42d
Jesus Aponte <jechuramirez1999@gmail.com>
2026-04-19 19:15:08 -0500

    fin seccion 2-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
+import { ItemComponent } from "./pages/item/item.component";
+import { AboutComponent } from "./pages/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, PortafolioComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PortafolioComponent, ItemComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Routes,RouterModule} from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
+export const routes: Routes = [
+    {path: 'home',component:PortafolioComponent},
+    {path: 'about',component:AboutComponent},
+    {path: 'item',component:ItemComponent},
+    {path: '**',pathMatch:'full',redirectTo: 'home'}
+];
