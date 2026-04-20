cb1abd1af7b3757f0415730174f8bb046e597868
Jesus Aponte <jechuramirez1999@gmail.com>
2026-04-20 16:17:24 -0500

    Seccion 5

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  constructor(public infoServiceEquipo:InfoPaginaService){
+    
+  }
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -2,19 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { InfoPagina } from '../interfaces/info-pagna.interface';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
   info:InfoPagina = {};
   cargada: boolean = false;
+  equipo: any[]=[];
 
   constructor(private http:HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json').subscribe((resp:InfoPagina) =>{
       this.cargada=true;
       this.info=resp;
-      console.log(resp)
     });
+  }
+  private cargarEquipo(){
+    this.http.get('https://angular-html-44327-default-rtdb.firebaseio.com/equipo.json').subscribe((resp:any) =>{
+      this.equipo = resp;
+    })
   }
 }
